6c8f48b4362f8ce5577adf70cbc336f55d86b6fc
Luis Garcia <lalejandro.1988@gmail.com>
2020-09-11 21:29:13 -0600

    Login con patron Bloc, InheritedWidget

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:form_validation/src/bloc/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final bloc = Provider.of(context);
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home')
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Email: ${ bloc.email }'),
+            Divider(),
+            Text('Password: ${ bloc.password }')
+          ],
+        ),
+      ),
+    );
+  }
+}

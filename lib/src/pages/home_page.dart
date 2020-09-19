940e9544778b97993e5529094f1ab25cdc483309
Luis Garcia <lalejandro.1988@gmail.com>
2020-09-19 11:34:34 -0600

    producto_page - Form

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:form_validation/src/bloc/provider.dart';
+// import 'package:form_validation/src/bloc/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final bloc = Provider.of(context);
+    // final bloc = Provider.of(context);
     
     return Scaffold(
       appBar: AppBar(
         title: Text('Home')
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Email: ${ bloc.email }'),
-            Divider(),
-            Text('Password: ${ bloc.password }')
-          ],
-        ),
+      body: Container(
+
       ),
+      floatingActionButton: _crearBoton(context),
+    );
+  }
+  _crearBoton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      backgroundColor: Colors.deepPurple,
+      onPressed: ()=> Navigator.pushNamed(context, 'producto'),
     );
   }
 }
